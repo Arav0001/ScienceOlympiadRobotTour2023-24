c7b0b6676a98f9fde69f983e87e949b77edcec0e
Arav Sonawane <88040397+Arav0001@users.noreply.github.com>
2024-01-15 20:34:30 -0500

    fixed pragma issue

--- src/DCMotor.h
+++ src/DCMotor.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@
 
 #include <string.h>
 
-#define LEFT_MOTOR 1
-#define RIGHT_MOTOR 2
+#define LEFT_MOTOR 3
+#define RIGHT_MOTOR 4
 
 #define LEFT_ENCODER_A 2
 #define LEFT_ENCODER_B 4
@@ -16,7 +16,7 @@
 
 #define WHEEL_DIAMETER 65.0f / 10.0f / 2.54f
 #define TICKS_PER_REVOLUTION 300
-#define TRACK_WIDTH 0.0f
+#define TRACK_WIDTH 6.0f
 
 DifferentialDrive drive(LEFT_MOTOR, RIGHT_MOTOR, LEFT_ENCODER_A, LEFT_ENCODER_B, RIGHT_ENCODER_A, RIGHT_ENCODER_B);
 
